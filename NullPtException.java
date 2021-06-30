1a0234db0ee1e95b53d746f1ea9a277bfe00faba
SahanaShanbhag03 <86728862+SahanaShanbhag03@users.noreply.github.com>
2021-06-30 23:26:57 +0530

    Create NullPtException.java

---
+++ NullPtException.java
@@ -0,0 +1,37 @@
+package com.company;
+
+class CustomException extends Exception
+{
+    CustomException()
+    {
+        System.out.println("Custom Exception has occurred");
+    }
+}
+public class NullPtException{
+    static void method2()
+    {
+        throw new NullPointerException();
+    }
+    static void method1() throws CustomException
+    {
+        try
+        {
+            method2();
+        }
+        catch(NullPointerException e)
+        {
+            throw new CustomException();
+        }
+    }
+    public static void main(String[] args)
+    {
+        try
+        {
+            method1();
+        }
+        catch(CustomException e)
+        {
+            System.out.println("NullPointerException has occurred");
+        }
+    }
+}
